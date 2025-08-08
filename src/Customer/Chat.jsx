d01b396dd68e3303c64bfe0cbbe09bc11589c4ae
saheer123-max <saheeraslam39@gmail.com>
2025-08-08 21:29:10 +0530

    first commit

--- src/Customer/Chat.jsx
+++ src/Customer/Chat.jsx
@@ -340,7 +340,7 @@ useEffect(() => {
                 onKeyPress={handleKeyPress}
                 placeholder="Type a message..."
                 className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                disabled={!isConnected....}
+                disabled={!isConnected}
               />
               <button
                 onClick={sendMessage}
